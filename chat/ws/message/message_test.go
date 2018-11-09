2f5bddbe387d1cb7f1bab6d218c56b5a8aebf3d2
adrian83 <adrianbrzoza1983@gmail.com>
2018-11-09 22:09:20 +0100

    few unit tests added

--- chat/ws/message/message_test.go
+++ chat/ws/message/message_test.go
@@ -1,26 +1,113 @@
 package message
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	unknownMessage = UnknownMessage{&Message{
+		MsgType:    "blabla",
+		SenderID:   "abc",
+		SenderName: "John",
+		Room:       "golang",
+	}}
+
+	joinRoomMessage = JoinRoomMessage{&Message{
+		MsgType:    userJoinedRoomMT,
+		SenderID:   "abc",
+		SenderName: "John",
+		Room:       "golang",
+	}}
+
+	leaveRoomMessage = LeaveRoomMessage{&Message{
+		MsgType:    userLeftRoomMT,
+		SenderID:   "def",
+		SenderName: "Jane",
+		Room:       "java",
+	}}
+)
 
 func TestUnknownMessageImplementsHandler(t *testing.T) {
 	var _ Handler = &UnknownMessage{}
 }
 
+func TestJoinRoomMessageImplementsHandler(t *testing.T) {
+	var _ Handler = &JoinRoomMessage{}
+}
+
+func TestLeaveRoomMessageImplementsHandler(t *testing.T) {
+	var _ Handler = &LeaveRoomMessage{}
+}
+
+func TestHandlerShouldInteractWithGivenRoomsAndSender(t *testing.T) {
+
+	var testData = []struct {
+		handler                      Handler
+		idExecuted                   int
+		sendExecuted                 int
+		addClientToRoomExecuted      int
+		createRoomExecuted           int
+		removeClientFromRoomExecuted int
+		sendMessageOnRoomExecuted    int
+	}{
+		{&unknownMessage, 0, 0, 0, 0, 0, 0},
+		{&joinRoomMessage, 0, 0, 1, 0, 0, 0},
+		{&leaveRoomMessage, 0, 0, 0, 0, 1, 0},
+	}
+
+	for _, data := range testData {
+
+		sender := &SenderStub{id: "abc"}
+		rooms := &RoomsStub{}
+
+		data.handler.DoWith(sender, rooms)
+
+		assert.Equal(t, sender.idExecuted, data.idExecuted, "Invalid number of sender.ID() calls")
+		assert.Equal(t, sender.sendExecuted, data.sendExecuted, "Invalid number of sender.Send(Message) calls")
+
+		assert.Equal(t, rooms.addClientToRoomExecuted, data.addClientToRoomExecuted, "Invalid number of rooms.AddClientToRoom(string, Sender) calls")
+		assert.Equal(t, rooms.createRoomExecuted, data.createRoomExecuted, "Invalid number of rooms.RemoveClientFromRoom(string, Sender) calls")
+		assert.Equal(t, rooms.removeClientFromRoomExecuted, data.removeClientFromRoomExecuted, "Invalid number of rooms.SendMessageOnRoom(Message) calls")
+		assert.Equal(t, rooms.sendMessageOnRoomExecuted, data.sendMessageOnRoomExecuted, "Invalid number of rooms.CreateRoom(string, Sender) calls")
+	}
+}
+
 type SenderStub struct {
-	id string
+	id           string
+	sendExecuted int
+	idExecuted   int
 }
 
 func (s *SenderStub) Send(msg Message) {
-
+	s.sendExecuted++
 }
+
 func (s *SenderStub) ID() string {
+	s.idExecuted++
 	return s.id
 }
 
 type RoomsStub struct {
+	addClientToRoomExecuted      int
+	removeClientFromRoomExecuted int
+	sendMessageOnRoomExecuted    int
+	createRoomExecuted           int
 }
 
-func (r *RoomsStub) AddClientToRoom(string, Sender)      {}
-func (r *RoomsStub) RemoveClientFromRoom(string, Sender) {}
-func (r *RoomsStub) SendMessageOnRoom(Message)           {}
-func (r *RoomsStub) CreateRoom(string, Sender)           {}
+func (r *RoomsStub) AddClientToRoom(string, Sender) {
+	r.addClientToRoomExecuted++
+}
+
+func (r *RoomsStub) RemoveClientFromRoom(string, Sender) {
+	r.removeClientFromRoomExecuted++
+}
+
+func (r *RoomsStub) SendMessageOnRoom(Message) {
+	r.sendMessageOnRoomExecuted++
+}
+
+func (r *RoomsStub) CreateRoom(string, Sender) {
+	r.createRoomExecuted++
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/dancannon/gorethink v4.0.0+incompatible
 	github.com/gorilla/mux v1.6.2
 	github.com/sirupsen/logrus v1.0.6
+	github.com/stretchr/testify v1.2.2
 	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b
 	golang.org/x/net v0.0.0-20180911220305-26e67e76b6c3
 	golang.org/x/sys v0.0.0-20180909124046-d0be0721c37e // indirect
